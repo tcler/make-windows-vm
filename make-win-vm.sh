def2ec738714a71ea180357ae2c75ab4c19b29ff
Jianhong.Yin <yin-jianhong@163.com>
2018-04-28 23:33:59 +0800

    all: optimize code w/o test
    remove global variables in functions, and simplify code

--- make-win-vm.sh
+++ make-win-vm.sh
@@ -1,101 +1,156 @@
 #!/bin/bash
 
-# For detailed comments and notes, visit https://wiki.test.redhat.com/Kernel/Filesystem/windows-demo
-
 # Include function
 . ./function
+PROG=${0##*/}
 
-
-# =======================================================================
-# Common Parameters, both KVM and windows use them
-# =======================================================================
-VM_MAC=$(gen_virt_mac)
-VM_EXT_MAC=$(gen_virt_mac 01)
-ANS_FILE_DIR="$(pwd)/answerfiles"
-DEFAULT_IF=$(ip -4 route get 1 | head -n 1 | awk '{print $5}')
-
-# =======================================================================
+# ==============================================================================
 # Parameter Processing
-# =======================================================================
-ARGS=`getopt -o bhi:n:p:t: \
-	--long bridge \
+# ==============================================================================
+Usage() {
+cat <<EOF
+Usage: $PEOG [OPTION]...
+
+  -h, --help    #Display this help.
+
+  --image </path/to/image>
+		#*Specify the path to windows image.
+  --product-key #Prodcut key for windows activation.
+  --hostname <hostname>
+		#hostname of windows
+  --domain <domain>
+		#*Specify windows domain name.
+  -u <user>     #Specify user for install and config, default: Administrator
+  -p <password> #*Specify user's password for windows.
+
+  --ad-forest-level
+		#Specify active directory forest level.
+  --ad-domain-level
+		#Specify active directory domain level.
+
+  --vm-name <VM_NAME>
+		#*Specify the vm guest's name.
+  --ram         #VM's ram size
+  --cpus        #Numbers of cpu cores for VM.
+  --disk-size   #VM disk size, in .qcow2 format.
+  --os-variant  #Specify os variant in for VM.
+  -t --ans-file-media-type <cdrom|floppy>
+		#Specify the answerfiles media type loaded to KVM.
+  -b, --bridge  #Use traditional bridge interface br0. Not recommended.
+  --timeout     #Set waiting timeout for installation.
+EOF
+}
+
+ARGS=$(getopt -o hu:p:t:b \
 	--long help \
-	--long vm-name: \
 	--long image: \
-	--long script-path: \
-	--long ans-file-media-type: \
-	--long admin-password: \
 	--long product-key: \
+	--long hostname: \
 	--long domain: \
 	--long ad-forest-level: \
 	--long ad-domain-level: \
+	--long vm-name: \
 	--long ram: \
 	--long cpus: \
 	--long disk-size: \
 	--long os-variant: \
+	--long ans-file-media-type: \
+	--long bridge \
 	--long timeout: \
-	-n "$0" -- "$@"`
+	-n "$PROG" -- "$@")
 eval set -- "$ARGS"
 while true; do
 	case "$1" in
-	-b|--bridge) BRIDGE=`setup_bridge`; shift 1;; 
 	-h|--help) Usage; exit 1;; 
-	-i|--image) WIN_ISO="$2"; shift 2;;
-	-n|--vm-name) VM_NAME="$2"; shift 2;;
-	-p|--script-path) EXTRA_SCRIPT="$2"; add_extra_script;shift 2;;
-	-t|--ans-file-media-type) ANSF_MEDIA_TYPE="$2"; shift 2;;
-	--admin-password) ADMINPASSWORD="$2"; shift 2;;
+	--image) WIN_ISO="$2"; shift 2;;
 	--product-key) PRODUCT_KEY="$2"; shift 2;;
+	--hostname) PRODUCT_KEY="$2"; shift 2;;
 	--domain) DOMAIN="$2"; shift 2;;
+	-u) ADMINNAME="$2"; shift 2;;
+	-p) ADMINPASSWORD="$2"; shift 2;;
 	--ad-forest-level) AD_FOREST_LEVEL="$2"; shift 2;;
 	--ad-domain-level) AD_DOMAIN_LEVEL="$2"; shift 2;;
+	--vm-name) VM_NAME="$2"; shift 2;;
 	--ram) VM_RAM="$2"; shift 2;;
 	--cpus) VM_CPUS="$2"; shift 2;;
 	--disk-size) VM_DISKSIZE="$2"; shift 2;;
 	--os-variant) VM_OS_VARIANT="$2"; shift 2;;
+	-t|--ans-file-media-type) ANSF_MEDIA_TYPE="$2"; shift 2;;
+	-b|--bridge) brgmode=yes; shift 1;; 
 	--timeout) VM_TIMEOUT="$2";shift 2;;
 	--) shift; break;;
 	*) Usage; exit 1;; 
 	esac
 done
 
+[[ -z "$WIN_ISO" || -z "$PRODUCT_KEY" || -z "$ADMINPASSWORD" || 
+   -z "$VM_NAME" ]] && {
+	Usage
+	exit 1
+}
+
+# =======================================================================
+# Global variable
+# =======================================================================
+IPCONFIG_LOG=ipconfig.log
+INSTALL_COMPLETE=installcomplete
+
 # =======================================================================
 # Windows Preparation
 # =======================================================================
-
-# Setup windows
+GUEST_HOSTNAME=${GUEST_HOSTNAME:-$VM_NAME}
+DOMAIN=${DOMAIN:-mytest.com}
 ADMINNAME=${ADMINNAME:-Administrator}
+ADMINPASSWORD=${ADMINPASSWORD:-password}
+VM_MAC=$(gen_virt_mac)
+VM_EXT_MAC=$(gen_virt_mac 01)
 DNS_IF_MAC=$VM_EXT_MAC
 MAC_DISABLE=$VM_MAC
 
 # Setup Active Directory
-FQDN=${FQDN:-$VM_NAME.$DOMAIN}
-NETBIOS_NAME=`echo $DOMAIN | sed -e 's/\.//g' | tr '[a-z]' '[A-Z]'`
+FQDN=${FQDN:-$GUEST_HOSTNAME.$DOMAIN}
+NETBIOS_NAME=$(echo ${DOMAIN//./} | tr '[a-z]' '[A-Z]')
 
 # =======================================================================
 # KVM Preparation
 # =======================================================================
-
 # Enable libvirtd remote access
 sed -i -e "/#libvirtd_args/s/^#//" /etc/sysconfig/libvirtd
 sed -i -e "/#listen_tls/s/^#//" /etc/libvirt/libvirtd.conf
 systemctl restart libvirtd.service
 systemctl restart virtlogd.service
 
+# ====================================================================
+# Setup_bridge
+# ====================================================================
+DEFAULT_IF=$(ip -4 route get 1 | head -n 1 | awk '{print $5}')
+[[ "$brgmode" = yes ]] && {
+	if [ $DEFAULT_IF != "br0" ]; then
+		local network_path="/etc/sysconfig/network-scripts"
+		echo -e "TYPE=Bridge\nBOOTPROTO=dhcp\nDEVICE=br0\nONBOOT=yes" \
+			> $network_path/ifcfg-br0
+		grep br0 $network_path/ifcfg-$DEFAULT_IF
+		if [ $? -ne 0 ]; then
+			echo "BRIDGE=br0" >> $network_path/ifcfg-$DEFAULT_IF
+		fi
+		systemctl restart network > /dev/null 2>&1
+	fi
+	local bridge="--network bridge=br0,model=rtl8139,mac=$VM_EXT_MAC"
+	echo $bridge
+}
+
 # Parameters
 VM_IMG_DIR=/var/lib/libvirt/images
 ANSF_CDROM=${ANSF_CDROM:-$VM_IMG_DIR/$VM_NAME-ansf-cdrom.iso}
 ANSF_FLOPPY=${ANSF_FLOPPY:-$VM_IMG_DIR/$VM_NAME-ansf-floppy.vfd}
-WIN_VM_DISKFILE=${WIN_VM_DISKFILE:-$VM_IMG_DIR/$VM_NAME.qcow2}
+VM_IMAGE=${VM_IMAGE:-$VM_IMG_DIR/$VM_NAME.qcow2}
 VM_NETWORK_NAME=default
 VM_IP=$(gen_ip)
-VM_EXT_IP=
-VIRTHOST=$(hostname -f)
-VM_WAIT_FILE="\\\\installcomplete"
-SERIAL_PATH=/tmp/serial-`date +'%Y%m%d%H%M%S'`.$$
 VM_NETWORK_INTERNAL="--network network=$VM_NETWORK_NAME,model=rtl8139,mac=$VM_MAC"
 VM_NETWORK_EXTRA="--network type=direct,source=$DEFAULT_IF,source_mode=vepa,mac=$VM_EXT_MAC"
 VM_NETWORK=${BRIDGE:-"$VM_NETWORK_INTERNAL $VM_NETWORK_EXTRA"}
+SERIAL_PATH=/tmp/serial-$(date +%Y%m%d%H%M%S).$$
+VIRTHOST=$(hostname -f)
 
 # Update KVM network configuration
 if [ -z "$BRIDGE" ]; then
@@ -103,12 +158,50 @@ if [ -z "$BRIDGE" ]; then
 	virsh net-update $VM_NETWORK_NAME add ip-dhcp-host "<host mac='"$VM_MAC"' name='"$VM_NAME"' ip='"$VM_IP"' />" --live --config
 fi
 
-# =======================================================================
-# Setup
-# =======================================================================
+# ====================================================================
+# Generate cdrom/floppy of answerfiles
+# ====================================================================
+process_ansf() {
+	local destdir=$1; shift
+	for f; do fname=${f##*/}; cp ${f} $destdir/${fname%.in}; done
 
-# Generate answerfiles cdrom/floppy
-make_ansf
+	sed -i -e "s/@ADMINPASSWORD@/$ADMINPASSWORD/g" \
+		-e "s/@ADMINNAME@/$ADMINNAME/g" \
+		-e "s/@AD_DOMAIN@/$DOMAIN/g" \
+		-e "s/@NETBIOS_NAME@/$NETBIOS_NAME/g" \
+		-e "s/@VM_NAME@/$VM_NAME/g" \
+		-e "s/@FQDN@/$FQDN/g" \
+		-e "s/@PRODUCT_KEY@/$PRODUCT_KEY/g" \
+		-e "s/@ANSF_DRIVE_LETTER@/$ANSF_DRIVE_LETTER/g" \
+		-e "s/@INSTALL_COMPLETE@/$INSTALL_COMPLETE/g" \
+		-e "s/@AD_FOREST_LEVEL@/$AD_FOREST_LEVEL/g" \
+		-e "s/@AD_DOMAIN_LEVEL@/$AD_DOMAIN_LEVEL/g" \
+		-e "s/@MAC_DISABLE@/$MAC_DISABLE/g" \
+		-e "s/@DNS_IF_MAC@/$DNS_IF_MAC/g" \
+		-e "s/@VIRTHOST@/$VIRTHOST/g" \
+		-e "s/@IPCONFIG_LOG@/$IPCONFIG_LOG/g" \
+		$destdir/*
+	unix2dos $destdir/*
+}
+
+rm -rf $ANSF_FLOPPY $ANSF_CDROM  #remove old/exist media file
+media_mp=$(mktemp -d)
+case "$ANSF_MEDIA_TYPE" in
+"floppy")
+	ANSF_MEDIA_PATH=$ANSF_FLOPPY
+	ANSF_DRIVE_LETTER="A:"
+	mkfs.vfat -C $ANSF_FLOPPY 1440 || { echo error $? from mkfs.vfat -C $ANSF_FLOPPY 1440; exit 1; }
+	mount -o loop -t vfat $ANSF_FLOPPY $media_mp
+	process_ansf $media_mp "$@"
+	umount $media_mp
+	;;
+"cdrom")
+	ANSF_MEDIA_PATH=$ANSF_CDROM
+	ANSF_DRIVE_LETTER="E:"
+	process_ansf $media_mp "$@"
+	genisoimage -iso-level 4 -J -l -R -o $ANSF_CDROM $media_mp
+	;;
+esac
 
 # Place libguestfs temporary files in properly labeled dir
 TMPDIR="/tmp/libguestfs"
@@ -117,38 +210,51 @@ mkdir $TMPDIR
 chcon -t svirt_tmp_t $TMPDIR
 
 # Execute virt-install command with the parameters given
-
-case "$ANSF_MEDIA_TYPE" in
-	"floppy") ANSF_MEDIA_PATH=$ANSF_FLOPPY;;
-	"cdrom") ANSF_MEDIA_PATH=$ANSF_CDROM;;
-esac
 virt-install --connect=qemu:///system --hvm \
 		--clock offset=utc \
 		--accelerate --name "$VM_NAME" --ram=$VM_RAM --vcpu=$VM_CPUS --cpu host,-invtsc \
 		--disk path=$WIN_ISO,device=cdrom \
 		--vnc --os-variant ${VM_OS_VARIANT} \
 		--serial file,path=$SERIAL_PATH --serial pty \
-		--disk path=$WIN_VM_DISKFILE,bus=ide,size=$VM_DISKSIZE,format=qcow2,cache=none \
+		--disk path=$VM_IMAGE,bus=ide,size=$VM_DISKSIZE,format=qcow2,cache=none \
 		--disk path=$ANSF_MEDIA_PATH,device=$ANSF_MEDIA_TYPE \
 		$VM_NETWORK \
 		|| { echo error $? from virt-install ; exit 1 ; }
 
 # To check whether the installation is done
-wait_for_completion
+success=no
+local t=${VM_TIMEOUT:-60}
+while ((t-- > 0)) ; do
+	virt-cat -d $VM_NAME -m /dev/sdb1 "/$INSTALL_COMPLETE" > /dev/null 2>&1 && {
+		success=yes; break;
+	}
+	sleep 1m
+done
+[[ $success != yes ]] && {
+	echo "Install timeout($VM_TIMEOUT)"
+}
 
 # =======================================================================
 # Post Setup
 # =======================================================================
-
-# Fetch the external ip address from windows
-# for machine from outside to reach it
-VM_EXT_IP=`get_external_ip`
-
 # When installation is done, test AD connection and get AD CA cert
 get_cert
 
-# Also save other variables into a log file
-save_ad_env
-
 # Eject CDs
-eject_cds
+eject_cds $VM_NAME  $WIN_ISO $ANSF_MEDIA_PATH
+
+# Save relative variables info a log file
+VM_EXT_IP=$(virt-cat -d $VM_NAME -m /dev/sdb1 /$IPCONFIG_LOG |
+	grep IPv4.Address | grep -v 192.168.122 |
+	egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+VM_INFO_FILE=/tmp/$VM_NAME.info
+cat <<- EOF | tee $VM_INFO_FILE
+	VM_NAME=$VM_NAME
+	VM_IP=$VM_IP
+	VM_EXT_IP=$VM_EXT_IP
+	ADMINNAME=$ADMINNAME
+	ADMINPASSWORD=$ADMINPASSWORD
+	DOMAIN=$DOMAIN
+	FQDN=$FQDN
+	NETBIOS_NAME=$NETBIOS_NAME
+EOF
