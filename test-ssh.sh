48a9ea1c4d7a25138d9655032d18e5e5bfa5865d
Jianhong.Yin <yin-jianhong@163.com>
2018-05-14 20:46:51 +0800

    remove hard code and redundant output

--- test-ssh.sh
+++ test-ssh.sh
@@ -2,15 +2,18 @@
 
 set timeout 120
 
-set ADMINUSER "$::env(NETBIOS_NAME)\\$::env(ADMINUSER)"
+set NETBIOSNAME "$::env(NETBIOS_NAME)"
+set ADMINUSER "$::env(ADMINUSER)"
+set LOGINUSER "${NETBIOSNAME}\\${ADMINUSER}"
 set PASSWD "$::env(ADMINPASSWORD)"
 set HOST "$::env(VM_INT_IP)"
-spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $ADMINUSER $HOST
+spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $LOGINUSER $HOST
 
+set pprefix [string tolower ${ADMINUSER}@${NETBIOSNAME}@]
 expect {
 	"password:" { send "${PASSWD}\r"; exp_continue }
-	">" { send "dir C:\\\r" }
+	-re "${pprefix}.*>" { send "dir C:\\\r" }
 }
-expect -re "administrator.*>" {
-	puts $expect_out(buffer)
+expect -re "${pprefix}.*>" {
+	puts ""
 }
