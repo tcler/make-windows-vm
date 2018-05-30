04c4e8d4838902aa4aae772f5811f8e508aa19f6
haoyouab <08826794brmt@gmail.com>
2018-05-30 17:11:22 +0800

    improve setup procedure and optimize
    * answerfiles/postinstall.ps1.in: Reload windows explorer after setup.
    * answerfiles-ad/postinstall.ps1.in: Likewise.
    * answerfiles-cifs-nfs/postinstall.ps1.in: Likewise.
    * test-ssh.sh: Remove unused variable NETBIOSNAME.

--- test-ssh.sh
+++ test-ssh.sh
@@ -2,14 +2,12 @@
 
 set timeout 120
 
-set NETBIOSNAME "$::env(NETBIOS_NAME)"
 set ADMINUSER "$::env(ADMINUSER)"
-set LOGINUSER "${NETBIOSNAME}\\${ADMINUSER}"
 set PASSWD "$::env(ADMINPASSWORD)"
 set HOST "$::env(VM_INT_IP)"
-spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $LOGINUSER $HOST
+spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l $ADMINUSER $HOST
 
-set pprefix [string tolower ${ADMINUSER}@${NETBIOSNAME}@]
+set pprefix [string tolower ${ADMINUSER}@]
 expect {
 	"password:" { send "${PASSWD}\r"; exp_continue }
 	-re "${pprefix}.*>" { send "dir C:\\\r" }
