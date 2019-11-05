695d8bc60c2ed85eacc541c217e5a05fa66b68c4
Jianhong.Yin <yin-jianhong@163.com>
2019-11-05 18:59:10 +0800

    test-cifs-nfs.sh: install dependancy
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- test-cifs-nfs.sh
+++ test-cifs-nfs.sh
@@ -7,6 +7,7 @@ host=$1
 }
 
 echo "{Info} get cifs share list ..."
+rpm -q samba-client &>/dev/null || yum install -y samba-client
 smbclient -m SMB3 -L $host -U guest%
 echo
 echo "{Info} get nfs share list ..."
