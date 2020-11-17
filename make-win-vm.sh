245bdb424fd645d39039c68869ed043e4952d26f
Jianhong Yin <jiyin@redhat.com>
2020-11-17 15:50:58 +0800

    add utils dir and move join-*.sh test*.sh into it
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- make-win-vm.sh
+++ make-win-vm.sh
@@ -555,14 +555,14 @@ EOF
 # Test SSH connection
 if [[ -n "$OpenSSHUrl" ]]; then
 	echo -e "\n{INFO} run follow command to test SSH connection"
-	echo "VM_INT_IP=$VM_INT_IP ADMINUSER=$ADMINUSER ADMINPASSWORD=$ADMINPASSWORD ./test-ssh.sh"
-	echo "VM_INT_IP=$VM_INT_IP ADMINUSER=$ADMINUSER ADMINPASSWORD=$ADMINPASSWORD ./test-ssh.sh"|bash|
+	echo "VM_INT_IP=$VM_INT_IP ADMINUSER=$ADMINUSER ADMINPASSWORD=$ADMINPASSWORD ./utils/test-ssh.sh"
+	echo "VM_INT_IP=$VM_INT_IP ADMINUSER=$ADMINUSER ADMINPASSWORD=$ADMINPASSWORD ./utils/test-ssh.sh"|bash|
 		sed -r -e 's|\x1b.[0-9]+;1H||' -e '/administrator@/s/ *(\x1b.2J)?(\x1b.[0-9]+;[0-9]+H){1,2}/ /' -e 's/ *\x1b[^ ]*.*$//'
 fi
 
 if [[ "$CHECK_AD" = yes ]]; then
 	echo -e "\n{INFO} run follow command to test AD connection"
 	ldapurl=ldap://${VM_INT_IP}
-	echo "./test-cert.sh $VM_NAME $FQDN $DOMAIN $ADMINUSER:$ADMINPASSWORD $ldapurl"
-	echo "./test-cert.sh $VM_NAME $FQDN $DOMAIN $ADMINUSER:$ADMINPASSWORD $ldapurl"|bash
+	echo "./utils/test-cert.sh $VM_NAME $FQDN $DOMAIN $ADMINUSER:$ADMINPASSWORD $ldapurl"
+	echo "./utils/test-cert.sh $VM_NAME $FQDN $DOMAIN $ADMINUSER:$ADMINPASSWORD $ldapurl"|bash
 fi
